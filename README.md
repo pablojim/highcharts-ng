b3f72927c1b3c19f4185d0e1db8d315f28ddc41b
pablojim <barfitzgerald@gmail.com>
2013-08-07 14:38:13 +0100

    Refactored to use config object Added xAxis control  Updated Example  Updated Docs

--- README.md
+++ README.md
@@ -5,9 +5,79 @@ AngularJS directive for Highcharts
 
 A simple Angularjs directive for Highcharts.
 
+Current Version
+---------------
+
+`<highchart id="chart1" config="chartConfig"></highchart>`
+
+The chart config resembles an exploded highcharts options object:
+
+`chartConfig = {
+             //Main highcharts options.
+             options: {
+                 chart: {
+                     type: 'bar'
+                 }
+             },
+             //Series object - a list of series using normal highcharts series options.
+             series: [{
+                 data: [10, 15, 12, 8, 7]
+             }],
+             //Title configuration
+             title: {
+                 text: 'Hello'
+             },
+             //Boolean to control showng loading status on chart
+             loading: false,
+             //Configuration for the xAxis. Currently only one x axis can be dynamically controlled.
+             //properties currentMin and currentMax provied 2-way binding to the chart's maximimum and minimum
+             xAxis: {
+              currentMin: 0,
+              currentMax: 20,
+              title: {text: 'values'}
+             }
+         }`
+
+All properties on the chart configuration are optional. Each property is watched for changes by angularjs.
+
+Features:
+---------
+
+- Adding and removing series
+- Setting/Updating Chart options
+- Updating the chart title
+- 2 way binding to chart xAxis
+- Control of Loading status
+- Resizes with screen size changes.
+
+
+Caveats:
+--------
+
+- Due to many equality checks the directive maybe slow with large datasets (this is solvable though...)
+- Whole Chart/Series is often redrawn where a simple update of data would suffice
+- If you don't assign ids to your series - incremental ids will be added
+- The 2 way binding to xAxis properties should be treated as experimental
+- When using with a highstocks navigator errors can occur
+- Needs tests!
+
+
+
+
+
+
+Older Versions
+--------------
+
+
+
+
+Version 0.0.1 (not compatible with currrent version)
+----------------
+
 `<highchart id="chart1" series="chart.series" title="chart.title" options="chart.options"></highchart>`
 
-See an example here: [http://jsfiddle.net/pablojim/Cp73s/](http://jsfiddle.net/pablojim/Cp73s/)
+See an example here: [http://jsfiddle.net/pablojim/46rhz/](http://jsfiddle.net/pablojim/46rhz/)
 
 Features:
 ---------
--- src/directives/highcharts-ng.js
+++ src/directives/highcharts-ng.js
@@ -1,94 +1,175 @@
 'use strict';
 
 angular.module('highcharts-ng', [])
-    .directive('highchart', function () {
-        var seriesId = 0;
-        var ensureIds = function (series) {
-            series.forEach(function (s) {
-                if (!angular.isDefined(s.id)) {
-                    s.id = "series-" + seriesId++;
-                }
+  .directive('highchart', function () {
+
+    function prependMethod(obj, method, func) {
+      var original = obj[method];
+      obj[method] = function () {
+        var args = Array.prototype.slice.call(arguments);
+        func.apply(this, args);
+        if(original) {
+          return original.apply(this, args);
+        }  else {
+          return;
+        }
+
+      };
+    }
+
+    var seriesId = 0;
+    var ensureIds = function (series) {
+      series.forEach(function (s) {
+        if (!angular.isDefined(s.id)) {
+          s.id = "series-" + seriesId++;
+        }
+      });
+    }
+
+    var defaultOptions = {
+      chart: {
+        events: {}
+      },
+      title: {},
+      series: [],
+      navigator: {enabled: false}
+    }
+
+    var getMergedOptions = function (scope, element, config) {
+      var mergedOptions = {}
+      if (config.options) {
+        mergedOptions = $.extend(true, {}, defaultOptions, angular.copy(config.options));
+      } else {
+        mergedOptions = defaultOptions;
+      }
+      mergedOptions.chart.renderTo = element[0];
+      if(scope.config.xAxis) {
+        prependMethod(mergedOptions.chart.events, 'selection', function(e){
+          var thisChart = this;
+          if(e.xAxis) {
+            scope.$apply(function () {
+              scope.config.xAxis.currentMin = e.xAxis[0].min;
+              scope.config.xAxis.currentMax = e.xAxis[0].max;
             });
+          } else {
+            //handle reset button - zoom out to all
+            scope.$apply(function () {
+              scope.config.xAxis.currentMin = thisChart.xAxis[0].dataMin;
+              scope.config.xAxis.currentMax = thisChart.xAxis[0].dataMax;
+            });
+          }
+        });
+
+        prependMethod(mergedOptions.chart.events, 'addSeries', function(e){
+            scope.config.xAxis.currentMin = this.xAxis[0].min || scope.config.xAxis.currentMin;
+            scope.config.xAxis.currentMax = this.xAxis[0].max || scope.config.xAxis.currentMax;
+        });
+      }
+
+      if(config.xAxis) {
+        mergedOptions.xAxis = angular.copy(config.xAxis)
+      }
+      if(config.title) {
+        mergedOptions.title = config.title
+      }
+      return mergedOptions
+    }
+
+    var updateZoom = function (axis, modelAxis) {
+      var extremes = axis.getExtremes();
+      if(modelAxis.currentMin !== extremes.dataMin || modelAxis.currentMax !== extremes.dataMax) {
+        axis.setExtremes(modelAxis.currentMin, modelAxis.currentMax, false);
+      }
+    }
+
+    var processExtremes = function(chart, axis) {
+      if(axis.currentMin || axis.currentMax) {
+        chart.xAxis[0].setExtremes(axis.currentMin, axis.currentMax, true);
+      }
+    }
+
+    var processSeries = function(chart, series) {
+      var ids = []
+      if(series) {
+        ensureIds(series);
+
+        //Find series to add or update
+        series.forEach(function (s) {
+          ids.push(s.id)
+          var chartSeries = chart.get(s.id);
+          if (chartSeries) {
+            chartSeries.update(angular.copy(s), false);
+          } else {
+            chart.addSeries(angular.copy(s), false)
+          }
+        });
+      }
+
+      //Now remove any missing series
+      chart.series.forEach(function (s) {
+        if (ids.indexOf(s.options.id) < 0) {
+          s.remove(false);
         }
+      });
+    }
 
-        var getMergedOptions = function (element, options, series) {
-            var defaultOptions = {
-                chart: {
-                    renderTo: element[0]
-                },
-                title: {},
-                series: []
-            }
-            var mergedOptions = {}
-            if (options) {
-                mergedOptions = $.extend(true, {}, defaultOptions, options);
-            } else {
-                mergedOptions = defaultOptions;
-            }
-            if(series) {
-              mergedOptions.series = series
-            }
-            return mergedOptions
-        }
-
-        return {
-            restrict: 'EC',
-            replace: false,
-            scope: {
-                series: '=',
-                options: '=',
-                title: '='
-            },
-            link: function (scope, element, attrs) {
-
-                var mergedOptions = getMergedOptions(element, scope.options, scope.series);
-                var chart = new Highcharts.Chart(mergedOptions);
-
-                scope.$watch("series", function (newSeries, oldSeries) {
-                    //do nothing when called on registration
-                    if (newSeries === oldSeries) return;
-                    if (newSeries) {
-                        ensureIds(newSeries);
-                        var ids = []
-
-                        //Find series to add or update
-                        newSeries.forEach(function (s) {
-                          ids.push(s.id)
-                          var chartSeries = chart.get(s.id);
-                          if (chartSeries) {
-                            chartSeries.update(angular.copy(s), false);
-                          } else {
-                            chart.addSeries(angular.copy(s), false)
-                          }
-                        });
-                        //Now remove any missing series
-                        chart.series.forEach(function (s) {
-                          if (ids.indexOf(s.options.id) < 0) {
-                            s.remove(false);
-                          }
-                        });
-                        chart.redraw();
-                    }
+    var initialiseChart = function(scope, element, config) {
+      var mergedOptions = getMergedOptions(scope, element, config);
+      var chart = new Highcharts.Chart(mergedOptions);
+      if(config.xAxis) {
+        processExtremes(chart, config.xAxis);
+      }
+      processSeries(chart, config.series);
+      chart.redraw();
+      return chart;
+    }
 
 
-                }, true);
-                scope.$watch("title", function (newTitle) {
-                    chart.setTitle(newTitle, true);
-                }, true);
-                scope.$watch("options", function (newOptions, oldOptions, scope) {
-                    //do nothing when called on registration
-                    if (newOptions === oldOptions) return;
-                    chart.destroy()
-                    var mergedOptions = getMergedOptions(element, newOptions);
-                    chart = new Highcharts.Chart(mergedOptions);
-                    chart.setTitle(scope.title, true);
-                    ensureIds(scope.series);
-                    scope.series.forEach(function (s) {
-                        chart.addSeries(angular.copy(s), false)
-                    });
-                    chart.redraw()
+    return {
+      restrict: 'EC',
+      replace: true,
+      template: '<div></div>',
+      scope: {
+        config: '='
+      },
+      link: function (scope, element, attrs) {
 
-                }, true);
-            }
-        }
-    });
+        var chart = initialiseChart(scope, element, scope.config);
+
+        scope.$watch("config.series", function (newSeries, oldSeries) {
+          //do nothing when called on registration
+          if (newSeries === oldSeries) return;
+          processSeries(chart, newSeries);
+          chart.redraw();
+        }, true);
+
+        scope.$watch("config.title", function (newTitle) {
+          chart.setTitle(newTitle, true);
+        }, true);
+
+        scope.$watch("config.loading", function (loading) {
+          if(loading === false) {
+            chart.hideLoading()
+          } else {
+            chart.showLoading()
+          }
+        }, true);
+
+        scope.$watch("config.xAxis", function (newAxes, oldAxes) {
+          if (newAxes === oldAxes) return;
+          if(newAxes) {
+            chart.xAxis[0].update(newAxes);
+            updateZoom(chart.xAxis[0], angular.copy(newAxes));
+            chart.redraw();
+          }
+        }, true);
+        scope.$watch("config.options", function (newOptions, oldOptions, scope) {
+          //do nothing when called on registration
+          if (newOptions === oldOptions) return;
+          chart.destroy();
+          chart = initialiseChart(scope, element, scope.config);
+
+        }, true);
+      }
+    }
+  });
