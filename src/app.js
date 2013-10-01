1d34461ab62bd3defc00e83509a578eb86890021
crusat <crusat@crusat.ru>
2013-10-01 17:32:21 +0400

    update view + added series

--- src/app.js
+++ src/app.js
@@ -31,8 +31,9 @@ myapp.controller('myctrl', function ($scope) {
 
     $scope.chartSeries = [
         {"name": "Some data", "data": [1, 2, 4, 7, 3]},
+        {"name": "Some data 3", "data": [3, 1, null, 5, 2], connectNulls: true},
         {"name": "Some data 2", "data": [5, 2, 2, 3, 5], type: "column"},
-        {"name": "Some data 3", "data": [3, 1, null, 5, 2], connectNulls: true}
+        {"name": "My Super Column", "data": [1, 1, 2, 3, 2], type: "column"}
     ];
 
     $scope.chartStack = [
