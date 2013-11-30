339d03f2298b5bf38dfa62785e726ef1a16d5b5b
pablojim <barfitzgerald@gmail.com>
2013-11-30 21:37:09 +0000

    Added example

---
+++ javascripts/app.js
@@ -0,0 +1,99 @@
+'use strict';
+
+var myapp = angular.module('myapp', ["highcharts-ng"]);
+
+myapp.controller('myctrl', function ($scope) {
+
+    $scope.chartTypes = [
+        {"id": "line", "title": "Line"},
+        {"id": "spline", "title": "Smooth line"},
+        {"id": "area", "title": "Area"},
+        {"id": "areaspline", "title": "Smooth area"},
+        {"id": "column", "title": "Column"},
+        {"id": "bar", "title": "Bar"},
+        {"id": "pie", "title": "Pie"},
+        {"id": "scatter", "title": "Scatter"}
+    ];
+
+    $scope.dashStyles = [
+        {"id": "Solid", "title": "Solid"},
+        {"id": "ShortDash", "title": "ShortDash"},
+        {"id": "ShortDot", "title": "ShortDot"},
+        {"id": "ShortDashDot", "title": "ShortDashDot"},
+        {"id": "ShortDashDotDot", "title": "ShortDashDotDot"},
+        {"id": "Dot", "title": "Dot"},
+        {"id": "Dash", "title": "Dash"},
+        {"id": "LongDash", "title": "LongDash"},
+        {"id": "DashDot", "title": "DashDot"},
+        {"id": "LongDashDot", "title": "LongDashDot"},
+        {"id": "LongDashDotDot", "title": "LongDashDotDot"}
+    ];
+
+    $scope.chartSeries = [
+        {"name": "Some data", "data": [1, 2, 4, 7, 3]},
+        {"name": "Some data 3", "data": [3, 1, null, 5, 2], connectNulls: true},
+        {"name": "Some data 2", "data": [5, 2, 2, 3, 5], type: "column"},
+        {"name": "My Super Column", "data": [1, 1, 2, 3, 2], type: "column"}
+    ];
+
+    $scope.chartStack = [
+        {"id": '', "title": "No"},
+        {"id": "normal", "title": "Normal"},
+        {"id": "percent", "title": "Percent"}
+    ];
+
+    $scope.addPoints = function () {
+        var seriesArray = $scope.chartConfig.series;
+        var rndIdx = Math.floor(Math.random() * seriesArray.length);
+        seriesArray[rndIdx].data = seriesArray[rndIdx].data.concat([1, 10, 20])
+    };
+
+    $scope.addSeries = function () {
+        var rnd = []
+        for (var i = 0; i < 10; i++) {
+            rnd.push(Math.floor(Math.random() * 20) + 1)
+        }
+        $scope.chartConfig.series.push({
+            data: rnd
+        })
+    }
+
+    $scope.removeRandomSeries = function () {
+        var seriesArray = $scope.chartConfig.series;
+        var rndIdx = Math.floor(Math.random() * seriesArray.length);
+        seriesArray.splice(rndIdx, 1)
+    }
+
+    $scope.removeSeries = function(id) {
+        var seriesArray = $scope.chartConfig.series;
+        seriesArray.splice(id, 1)
+    }
+
+    $scope.toggleHighCharts = function () {
+        this.chartConfig.useHighStocks = !this.chartConfig.useHighStocks
+    }
+
+    $scope.chartConfig = {
+        options: {
+            chart: {
+                type: 'areaspline'
+            },
+            plotOptions: {
+                series: {
+                    stacking: ''
+                }
+            }
+        },
+        series: $scope.chartSeries,
+        title: {
+            text: 'Hello'
+        },
+        credits: {
+            enabled: true
+        },
+        loading: false
+    }
+
+
+
+});
