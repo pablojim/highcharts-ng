2c9fe2b7191ad1e5b235a3c517b01f4a8981d51b
pablojim <barfitzgerald@gmail.com>
2013-11-30 23:35:18 +0000

    Refactor to use grunt Now we can add some tests!

---
+++ Gruntfile.js
@@ -0,0 +1,193 @@
+// Generated on 2013-11-30 using generator-angular-component 0.2.3
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Configurable paths
+  var yoConfig = {
+    livereload: 35729,
+    src: 'src',
+    dist: 'dist'
+  };
+
+  // Livereload setup
+  var lrSnippet = require('connect-livereload')({port: yoConfig.livereload});
+  var mountFolder = function (connect, dir) {
+    return connect.static(require('path').resolve(dir));
+  };
+
+  // Load all grunt tasks
+  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+  // Project configuration
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    yo: yoConfig,
+    meta: {
+      banner: '/**\n' +
+      ' * <%= pkg.name %>\n' +
+      ' * @version v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %>\n' +
+      ' * @link <%= pkg.homepage %>\n' +
+      ' * @author <%= pkg.author.name %> <<%= pkg.author.email %>>\n' +
+      ' * @license MIT License, http://www.opensource.org/licenses/MIT\n' +
+      ' */\n'
+    },
+    open: {
+      server: {
+        path: 'http://localhost:<%= connect.options.port %>'
+      }
+    },
+    clean: {
+      dist: {
+        files: [{
+          dot: true,
+          src: [
+            '.tmp',
+            '<%= yo.dist %>/*',
+            '!<%= yo.dist %>/.git*'
+          ]
+        }]
+      },
+      server: '.tmp'
+    },
+    watch: {
+      gruntfile: {
+        files: '<%= jshint.gruntfile.src %>',
+        tasks: ['jshint:gruntfile']
+      },
+      less: {
+        files: ['<%= yo.src %>/{,*/}*.less'],
+        tasks: ['less:dist']
+      },
+      app: {
+        files: [
+          '<%= yo.src %>/{,*/}*.html',
+          '{.tmp,<%= yo.src %>}/{,*/}*.css',
+          '{.tmp,<%= yo.src %>}/{,*/}*.js'
+        ],
+        options: {
+          livereload: yoConfig.livereload
+        }
+      },
+      test: {
+        files: '<%= jshint.test.src %>',
+        tasks: ['jshint:test', 'qunit']
+      }
+    },
+    connect: {
+      options: {
+        port: 9000,
+        hostname: '0.0.0.0' // Change this to '0.0.0.0' to access the server from outside.
+      },
+      livereload: {
+        options: {
+          middleware: function (connect) {
+            return [
+              lrSnippet,
+              mountFolder(connect, '.tmp'),
+              mountFolder(connect, yoConfig.src)
+            ];
+          }
+        }
+      }
+    },
+    less: {
+      options: {
+        // dumpLineNumbers: 'all',
+        paths: ['<%= yo.src %>']
+      },
+      dist: {
+        files: {
+          '<%= yo.src %>/<%= yo.name %>.css': '<%= yo.src %>/<%= yo.name %>.less'
+        }
+      }
+    },
+    jshint: {
+      gruntfile: {
+        options: {
+          jshintrc: '.jshintrc'
+        },
+        src: 'Gruntfile.js'
+      },
+      src: {
+        options: {
+          jshintrc: '.jshintrc'
+        },
+        src: ['<%= yo.src %>/{,*/}*.js']
+      },
+      test: {
+        options: {
+          jshintrc: 'test/.jshintrc'
+        },
+        src: ['test/**/*.js']
+      }
+    },
+    karma: {
+      options: {
+        configFile: 'karma.conf.js',
+        browsers: ['PhantomJS']
+      },
+      unit: {
+        singleRun: true
+      },
+      server: {
+        autoWatch: true
+      }
+    },
+    ngmin: {
+      options: {
+        banner: '<%= meta.banner %>'
+      },
+      dist: {
+        src: ['<%= yo.src %>/<%= pkg.name %>.js'],
+        dest: '<%= yo.dist %>/<%= pkg.name %>.js'
+      }
+      // dist: {
+      //   files: {
+      //     '/.js': '/.js'
+      //   }
+      // }
+    },
+    concat: {
+      options: {
+        banner: '<%= meta.banner %>',
+        stripBanners: true
+      },
+      dist: {
+        src: ['<%= yo.src %>/<%= pkg.name %>.js'],
+        dest: '<%= yo.dist %>/<%= pkg.name %>.js'
+      }
+    },
+    uglify: {
+      options: {
+        banner: '<%= meta.banner %>'
+      },
+      dist: {
+        src: '<%= concat.dist.dest %>',
+        dest: '<%= yo.dist %>/<%= pkg.name %>.min.js'
+      }
+    }
+  });
+
+  grunt.registerTask('test', [
+    'jshint',
+    'karma:unit'
+  ]);
+
+  grunt.registerTask('build', [
+    'clean:dist',
+    'less:dist',
+    'ngmin:dist',
+    'uglify:dist'
+  ]);
+
+  grunt.registerTask('release', [
+    'test',
+    'bump-only',
+    'dist',
+    'bump-commit'
+  ]);
+
+  grunt.registerTask('default', ['build']);
+
+};
---
+++ karma.conf.js
@@ -0,0 +1,69 @@
+// Karma configuration
+module.exports = function(config) {
+  config.set({
+
+    // base path, that will be used to resolve files and exclude
+    basePath: '',
+
+    preprocessors: {
+      'app/scripts/**/*.js': 'coverage'
+    },
+
+    frameworks: ['jasmine'],
+
+    // list of files / patterns to load in the browser
+    files: [
+      'bower_components/angular/angular.js',
+      'bower_components/jquery/jquery.js',
+      'bower_components/angular-mocks/angular-mocks.js',
+      'src/*.js',
+      'test/spec/*.js'
+    ],
+
+    // list of files to exclude
+    exclude: [],
+
+    // test results reporter to use
+    // possible values: dots || progress || growl
+    reporters: ['progress', 'coverage'],
+
+    // web server port
+    port: 8080,
+
+    // cli runner port
+    runnerPort: 9100,
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+    // level of logging
+    // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
+    logLevel: 'LOG_INFO',
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: false,
+
+    // Start these browsers, currently available:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera
+    // - Safari (only Mac)
+    // - PhantomJS
+    // - IE (only Windows)
+    browsers: ['Chrome'],
+
+    // If browser does not capture in given timeout [ms], kill it
+    captureTimeout: 5000,
+
+    // Continuous Integration mode
+    // if true, it capture browsers, run tests and exit
+    singleRun: false,
+
+    coverageReporter: {
+      type: 'html',
+      dir: 'coverage/'
+    }
+
+  });
+};
